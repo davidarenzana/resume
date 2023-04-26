0acb596a76c27103388de9ba1e326032d0575ef6
David Arenzana <davidarenzana@calidae.com>
2023-04-26 07:45:44 +0200

    config: add configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  ssr: true,
+  typescript: {
+    typeCheck: true,
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'David Arenzana - Curriculum Vitae',
+      meta: [
+        {
+          name: 'Este soy yo, mi CV - David Arenzana',
+          content:
+            'Si tienes curiosidad por saber de trayectoria profesional, aquí tienes un pequeño resumen',
+        },
+      ],
+    },
+  },
+  css: ['@/assets/main.css'],
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <h1>Hola!</h1>
+  </header>
+  <aside>sidebar</aside>
+  <main>content</main>
+</template>
+
+<style lang="scss" scoped></style>
