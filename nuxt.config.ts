dfbd89101ee70e9c28e4dba2c33fd4ff68d61be4
David Arenzana <davidarenzana@calidae.com>
2023-04-28 10:49:31 +0200

    config: i18n configuration and define locales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,37 @@
+import eslintPlugin from 'vite-plugin-eslint'
+
 export default defineNuxtConfig({
-  ssr: true,
-  typescript: {
-    typeCheck: true,
-  },
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'David Arenzana - Curriculum Vitae',
-      meta: [
-        {
-          name: 'Este soy yo, mi CV - David Arenzana',
-          content:
-            'Si tienes curiosidad por saber de trayectoria profesional, aquí tienes un pequeño resumen',
-        },
-      ],
+      title: 'Calidae · Digital Craftsmanship',
+      meta: [{ name: 'description', content: 'My amazing site.' }],
     },
   },
+
+  components: [
+    {
+      path: '~/components/',
+      pathPrefix: false,
+    },
+  ],
+
   css: ['@/assets/main.css'],
+
+  // @ts-ignore
+  modules: ['@nuxtjs/i18n'],
+
+  i18n: {
+    strategy: 'prefix_except_default',
+    locales: [
+      { code: 'es', iso: 'es-ES', name: 'Español' },
+      { code: 'ca', iso: 'ca-ES', name: 'Català' },
+    ],
+    defaultLocale: 'es',
+  },
+  vite: {
+    plugins: [eslintPlugin()],
+  },
+  ssr: true,
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+</script>
+
+
 <template>
-  <header>
-    <h1>Hola!</h1>
-  </header>
-  <aside>sidebar</aside>
-  <main>content</main>
+  <div>
+    <header>
+      <h1>{{ t('hello') }}</h1>
+
+    </header>
+    <aside>sidebar</aside>
+    <main>content</main>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
+
+<i18n lang="yml">
+
+  es: 
+    "hello": "Hola world!"
+  
+  ca: 
+    "hello": "hello mon"
+  
+
+</i18n>
